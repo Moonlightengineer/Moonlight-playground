433b03b936b7a254b325eddacacd638e072f0439
Moonlight Engineer <kenchan@sng-eng.com>
2026-07-28 20:16:24 +0800

    fix(hanzi-v2): preserve all owner zones when rebuilding registry

--- games/hanzi-generals/v2/src/expedition/rewards.js
+++ games/hanzi-generals/v2/src/expedition/rewards.js
@@ -41,8 +41,12 @@ export function generateRewardChoices(game, catalogue, rng) {
 
 function rebuildCardsById(game, deck) {
   const cards = [...deck.drawPile, ...deck.discardPile, ...deck.hand];
-  const deployedIds = new Set(deck.deployed.flatMap(({ cardIds }) => cardIds));
-  for (const id of deployedIds) {
+  const ownerIds = new Set([
+    ...(game.camp?.cardIds ?? []),
+    ...Object.values(game.boardCards ?? {}),
+    ...deck.deployed.flatMap(({ cardIds }) => cardIds),
+  ]);
+  for (const id of ownerIds) {
     if (game.cardsById[id]) cards.push(game.cardsById[id]);
   }
   return Object.fromEntries(cards.map((card) => [card.id, card]));
